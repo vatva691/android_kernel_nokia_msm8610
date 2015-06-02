8d4154f27fb0b0b6da8682be1c101df61b9c3bfa
zombah <zombah@gmail.com>
2015-06-02 23:37:27 +0300

    Revert "ASoC: wcd9xxx: Make recalibration configurable"
    This reverts commit 4b5159f8902ace81b13e6013674be6dcf86c1a16.

--- sound/soc/codecs/wcd9xxx-mbhc.h
+++ sound/soc/codecs/wcd9xxx-mbhc.h
@@ -228,7 +228,6 @@ struct wcd9xxx_mbhc_config {
 	bool (*swap_gnd_mic) (struct snd_soc_codec *);
 	unsigned long cs_enable_flags;
 	bool use_int_rbias;
-	bool do_recalibration;
 };
 
 struct wcd9xxx_cfilt_mode {
